a62a325f1378e09e4b5ddefbab260b0342c94a75
MIROSLAVSH <sharkovskiymiroslav@gmail.com>
2023-05-16 12:21:04 +0300

    first commit

---
+++ components/Preloader.js
@@ -0,0 +1,5 @@
+import { BounceLoader } from 'react-spinners';
+
+export default function Preloader() {
+	return (<BounceLoader speenMultiplier={2} color={'#348dfa'} />)
+}
---
+++ pages/profile/[id].js
@@ -0,0 +1,7 @@
+import { useRouter } from "next/router";
+import ProfilePage from "../profile";
+
+export default function Id(){
+	
+	return <ProfilePage />
+}
